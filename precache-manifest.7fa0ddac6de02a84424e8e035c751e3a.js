08427bfed3d957cc85a0dbea1e1ad0bf51079c07
Enot Hueda <enot.hueda@gmail.com>
2019-12-02 20:54:50 -0600

    Updates

---
+++ precache-manifest.7fa0ddac6de02a84424e8e035c751e3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2b8a83a1cdf194ddcb093adbdbc53a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ad162abcdec75b627884",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f5eab55efce67fed0769",
+    "url": "/robofriends/static/css/main.c70499b4.chunk.css"
+  },
+  {
+    "revision": "ad162abcdec75b627884",
+    "url": "/robofriends/static/js/2.42da0895.chunk.js"
+  },
+  {
+    "revision": "f5eab55efce67fed0769",
+    "url": "/robofriends/static/js/main.44e1bc07.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
